1a5e9c3b4f41d4ffea3bd2ecde7429da6fa56cf1
Jon Black <jblack@JolleCocoon.local>
2017-01-17 17:00:02 -0500

    working on iterating through many roves

--- run.rb
+++ run.rb
@@ -2,15 +2,19 @@ require_relative 'drunkard'
 
 puts 'Welcome to the Rover'
 
-loop do
-  d = Drunkard.new
-  puts 'Please enter directions in any combination of "L", "R", or "F" '
-  print '>'
+100000.times do
+    d = Drunkard.new
+    iterator = 0
+    directions_string = "FFLLFLLFLLFLLTRRRRLFRFRFFFFFFFFLFFFFFFFFFLLLLLLFFFFFFLLLLLFFFFFFFLLLLLLRLRLRLRLRLFLRLFLRLFLFFFFLLLRFFFFF"
+    shuffled_string = directions_string.chars.shuffle.join('')
+    d.move_and_turn(shuffled_string)
+    if (d.position_x == 2) && (d.position_y == 2)
+      iterator += 1
+    end
 
-  directions = gets.chomp
-  d.move_and_turn(directions)
-
-  puts "Your X position is #{d.position_x}"
-  puts "Your Y position is #{d.position_y}"
+    puts "you have been through (0, 0) #{iterator} times"
 
 end
+
+  # puts "Your X position is #{d.position_x}"
+  # puts "Your Y position is #{d.position_y}"
