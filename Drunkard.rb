cffa7ba72f3d1eb1739d3e39d2179d29392e6c8d
Jon Black <jblack@JolleCocoon.local>
2017-01-17 15:49:44 -0500

    turning works

--- Drunkard.rb
+++ Drunkard.rb
@@ -1,6 +1,71 @@
 class Drunkard
+
+  attr_reader :position_x, :position_y
+
   def initialize
-    @position = {x: 0, y: 0}
+    @position_x = 0
+    @position_y = 0
+    @current_direction = 'north'
+  end
+  # def move_and_turn(directions_string)
+  #   x = directions_string.upcase
+  #   @directions_array = x.split(',') #[R, F, L, L, F]
+  #
+  #   @directions_array.each do |letter|
+  #         if letter == 'F'
+  #           move_forward
+  #         else
+  #           turn_left_or_right(letter)
+  #         end
+  #       end
+  # end
+  # # def accept_directions(directions_string)
+  # #   x = directions_string.upcase
+  # #   @direction_array = x.split(',') #[R, F, L, L, F]
+  # # end
+  # # def self.perform_directions(directions_array, drunkard)
+  # #   directions_array.each do |letter|
+  # #     if letter == 'F'
+  # #       drunkard.move_forward
+  # #     else
+  # #       drunkard.turn_left_or_right(letter)
+  # #     end
+  # #   end
+  # # end
+
+  def move_forward
+    if @current_direction == 'north'
+      @position_y += 1
+    end
+    if @current_direction == 'south'
+      @position_y -= 1
+    end
+    if @current_direction == 'east'
+      @position_x += 1
+    end
+    if @current_direction == 'west'
+      @position_x -= 1
+    end
   end
 
+  def turn_left_or_right(direction_of_turn)
+
+    if (@current_direction == 'north') && (direction_of_turn == 'R')
+      @current_direction = 'east'
+    elsif (@current_direction == 'north') && (direction_of_turn == 'L')
+      @current_direction = 'west'
+    elsif @current_direction == 'east' && direction_of_turn == 'R'
+      @current_direction = 'south'
+    elsif @current_direction == 'east' && direction_of_turn == 'L'
+      @current_direction = 'north'
+    elsif @current_direction == 'south' && direction_of_turn == 'R'
+      @current_direction = 'west'
+    elsif @current_direction == 'south' && direction_of_turn == 'L'
+      @current_direction = 'east'
+    elsif @current_direction == 'west' && direction_of_turn == 'R'
+      @current_direction = 'north'
+    elsif @current_direction == 'west' && direction_of_turn == 'L'
+      @current_direction = 'south'
+    end
+  end
 end
--- drunkard_spec.rb
+++ drunkard_spec.rb
@@ -3,8 +3,25 @@ require_relative  'drunkard'
 
 describe Drunkard do
   it 'can move forward one space' do
-    #check to see if
-  end
-  it 'can move forward one space, change direction (left), and move forward one more space'
+    d = Drunkard.new
+    expect(d.position_x).to eq 0
+    expect(d.position_y).to eq 0
 
+    d.move_forward
+
+    expect(d.position_x).to eq 0
+    expect(d.position_y).to eq 1
+  end
+
+  it 'can move forward one space, change direction (right), and move forward one more space' do
+    d = Drunkard.new
+
+    d.move_forward
+    expect(d.position_y).to eq 1
+
+    d.turn_left_or_right('L')
+    d.move_forward
+
+    expect(d.position_x).to eq -1
+  end
 end
