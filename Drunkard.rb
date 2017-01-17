c711334934753fa635192c5e5c48832dd30cdc12
Jon Black <jblack@JolleCocoon.local>
2017-01-17 14:26:02 -0500

    started spec

--- Drunkard.rb
+++ Drunkard.rb
@@ -1,3 +1,6 @@
 class Drunkard
+  def initialize
+    @position = {x: 0, y: 0}
+  end
 
 end
---
+++ drunkard_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative  'drunkard'
+
+describe Drunkard do
+  it 'can move forward one space' do
+    #check to see if
+  end
+  it 'can move forward one space, change direction (left), and move forward one more space'
+
+end
